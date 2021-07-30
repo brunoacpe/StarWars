f36096f685cc7df9d33132af9c8fd4d0b4b9a857
vitoriabentes <vitoriabentes04@gmail.com>
2021-07-30 19:52:29 -0300

    Excluindo comentários e importações não utilizadas

--- src/main/java/br/com/letscode/Controller/NegociarItemController.java
+++ src/main/java/br/com/letscode/Controller/NegociarItemController.java
@@ -1,7 +1,5 @@
 package br.com.letscode.Controller;
 import br.com.letscode.Model.ArrayDeRecursos;
-import br.com.letscode.Model.Rebelde;
-import br.com.letscode.Model.Recursos;
 import br.com.letscode.Services.NegociarItensService;
 import br.com.letscode.Services.RebeldesServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/negociar")
@@ -44,24 +38,4 @@ public class NegociarItemController {
     public String negociarItens(@RequestBody ArrayDeRecursos itensDeTrocaRebelde1, @RequestParam("nomeRebelde1") String nomeRebelde1, @RequestParam("nomeRebelde2") String nomeRebelde2) {
         return negociarItensService.negociarItens(itensDeTrocaRebelde1, nomeRebelde1, nomeRebelde2);
     }
-    //TODO    n sabia como deixar outra marcaçao pra chamar a atençao mas esse é o jeito q deve ser passado os recursos da troca
-    //TODO    os recursos1 é pro rebelde1 e o recursos2 pro rebelde2 testado e funcionado ate o momento :)
-
-//        {
-//            "recursos1": {
-//            "arma": 1,
-//                    "municao": 2,
-//                    "agua": 1,
-//                    "comida": 0
-//        },
-//            "recursos2": {
-//            "arma": 1,
-//                    "municao": 2,
-//                    "agua": 1,
-//                    "comida": 0
-//        }
-//        }
-
-
-     //TODO TESTAR O CÓDIGO obs:está dando bug no requestParam dos itens
 }
--- src/main/java/br/com/letscode/Model/ArrayDeRecursos.java
+++ src/main/java/br/com/letscode/Model/ArrayDeRecursos.java
@@ -2,10 +2,7 @@ package br.com.letscode.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
